8b1b6a16e4d4b7db93b073e13d8da1211add04fe
Joel Santiago <jsant288@fiu.edu>
2021-09-23 06:59:25 +0000

    created excuse generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = excuseGenerator();
 };
+function excuseGenerator() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoIndex = Math.floor(Math.random() * who.length);
+  return (
+    who[whoIndex] +
+    " " +
+    action[whoIndex] +
+    " " +
+    what[whoIndex] +
+    " " +
+    when[whoIndex]
+  );
+}
