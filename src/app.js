0bd8d8028dfa9fc0029a9110cd74a3f4a57b9c50
Joel Santiago <jsant288@fiu.edu>
2021-09-27 04:23:04 +0000

    reorganized js file and created new function for random element for arrays

--- src/app.js
+++ src/app.js
@@ -1,33 +1,35 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
 window.onload = function() {
   //write your code here
-  document.querySelector("#excuse").innerHTML = excuseGenerator();
+  document.querySelector("#excuse").innerHTML = randomPartOfArray();
+  var myButton = document.getElementById("button");
 };
-function excuseGenerator() {
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
-  let whoIndex = Math.floor(Math.random() * who.length);
-  return (
-    who[whoIndex] +
-    " " +
-    action[whoIndex] +
-    " " +
-    what[whoIndex] +
-    " " +
-    when[whoIndex]
-  );
+
+function randomPartOfArray(arrayName) {
+  return arrayName[Math.floor(Math.random() * arrayName.length)];
 }
+console.log(randomPartOfArray(who));
+console.log(randomPartOfArray(action));
+console.log(randomPartOfArray(what));
+console.log(randomPartOfArray(when));
+
+// function joelExcuseSentence(array) {
+//   return array[Math.floor(Math.random() * array.length)];
+// }
+// // excuseGenerator(who);
+// // excuseGenerator(what);
+// // excuseGenerator(action)
+// console.log(joelExcuseSentence(who));
+// console.log(joelExcuseSentence(action));
+// console.log(joelExcuseSentence(what));
+// console.log(joelExcuseSentence(when));
