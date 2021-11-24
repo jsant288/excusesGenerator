4c838dd5a51ed4e3b68fa3ecc6a272785181ce92
Joel Santiago <jsant288@fiu.edu>
2021-11-24 19:22:49 +0000

    fixbugs

--- src/app.js
+++ src/app.js
@@ -1,35 +1,27 @@
-let who = ["The dog", "My grandma", "His turtle", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "the keys", "the car"];
-let when = [
-  "before the class",
-  "right on time",
-  "when I finished",
-  "during my lunch",
-  "while I was praying"
-];
-
 window.onload = function() {
-  //write your code here
-  document.querySelector("#excuse").innerHTML = randomPartOfArray();
-  var myButton = document.getElementById("button");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  document
+    .querySelector("#myButton")
+    .addEventListener("click", randomPartOfArray);
+  function randomPartOfArray() {
+    return (document.querySelector("#excuse").innerHTML =
+      who[Math.floor(Math.random() * who.length)] +
+      " " +
+      action[Math.floor(Math.random() * action.length)] +
+      " " +
+      what[Math.floor(Math.random() * what.length)] +
+      " " +
+      when[Math.floor(Math.random() * when.length)]);
+  }
+  randomPartOfArray();
 };
-
-function randomPartOfArray(arrayName) {
-  return arrayName[Math.floor(Math.random() * arrayName.length)];
-}
-console.log(randomPartOfArray(who));
-console.log(randomPartOfArray(action));
-console.log(randomPartOfArray(what));
-console.log(randomPartOfArray(when));
-
-// function joelExcuseSentence(array) {
-//   return array[Math.floor(Math.random() * array.length)];
-// }
-// // excuseGenerator(who);
-// // excuseGenerator(what);
-// // excuseGenerator(action)
-// console.log(joelExcuseSentence(who));
-// console.log(joelExcuseSentence(action));
-// console.log(joelExcuseSentence(what));
-// console.log(joelExcuseSentence(when));
